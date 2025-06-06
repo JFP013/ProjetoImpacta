2d5723232028152bb748790f3df871606ebd3668
Jefferson Ferreira da Piedade <jfdapiedade@gmail.com>
2025-06-06 02:13:19 -0300

    Implementação da função de delete

---
+++ ProjetoImpacta/Pages/Clientes/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjetoImpacta.Pages.Clientes
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
